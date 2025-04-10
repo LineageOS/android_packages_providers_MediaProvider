8a76b07bf561d2a5529298c29971f879c1ce1076
nift4 <nift4@protonmail.com>
2025-04-10 21:17:26 +0200

    Add missing permission checks for AUDIO_ALBUMART_FILE_ID
    Because checking for AUDIO_ALBUMART_FILE_ID was forgotten in the
    AccessChecker, the URI entered an inconsistent state where apps
    could open it, but couldn't grant URI permissions. This was a problem
    when using this URI together with MediaSession.setMetadata() as that
    requires URI sharing permission.
    
    Other album art URIs already support granting URI permissions, just the
    per-file album art type was forgotten from this switch-case statement.
    
    Bug: 409809745
    Change-Id: I6c247bf26e828a01cc3c83503ae80343fbe444e7

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -3768,6 +3768,15 @@ public class MediaProvider extends ContentProvider {
             final boolean allowHidden = isCallingPackageAllowedHidden();
             final int table = matchUri(uri, allowHidden);
 
+            if (table == AUDIO_ALBUMART_FILE_ID) {
+                final String volumeName = getVolumeName(uri);
+                final long albumId = Long.parseLong(uri.getPathSegments().get(3));
+                final Uri targetUri = ContentUris
+                        .withAppendedId(Audio.Media.getContentUri(volumeName), albumId);
+                return getContext().checkUriPermission(targetUri, mCallingIdentity.get().pid,
+                        mCallingIdentity.get().uid, modeFlags);
+            }
+
             final DatabaseHelper helper;
             try {
                 helper = getDatabaseForUri(uri);

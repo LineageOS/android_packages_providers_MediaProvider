99ee8cb3aaabd60aa4d0aebce8e5b07448dd71c5
darshilshah <darshilshah@google.com>
2025-05-15 14:45:41 +0000

    [security-aosp-25Q2-staging] Remove default ignoreable characters before regex checks
    This is a combined CL for cherry-pick. Original CLs merged in main-
    ag/33577707
    ag/33920560
    ag/33920560
    
    Lower file system ignores certain characters while saving files.
    Malicious apps use these ignorable characters to by-pass regex checks
    that are needed to protect private app directories. We remove these
    characters before regex checks so that apps cannot by-pass the checks.
    
    Bug: 377888957
    Test: atest com.android.providers.media.util.FileUtilsTest#testNormalizeAndFilterDefaultIgnorableCodepoints
    Test: atest com.android.providers.media.util.FileUtilsTest#testIsDataOrObbRelativePathWithZwsChars_flagEnabled
    Test: atest com.android.providers.media.util.FileUtilsTest#testIsDataOrObbRelativePathWithZwsChars_flagDisabled
    Flag: com.android.providers.media.flags.enable_path_sanitization
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:ac3a6dc8db818a74357af1dfa2acfa911648827c)
    Change-Id: Ibb96bda4b4fdb0869209aeb37260bf656d155a9e

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -10502,7 +10502,7 @@ public class MediaProvider extends ContentProvider {
         // Check if the caller has access to private app directories. Checks for Android/data,
         // Android/media and Android/obb
         boolean isUidAllowedAccessToDataOrObbPath =
-                isUidAllowedAccessToDataOrObbPathForFuse(mCallingIdentity.get().uid, filePath);
+                isUidAllowedAccessToDataOrObbPath(mCallingIdentity.get().uid, filePath);
 
         /*
          * If owned photos is enabled, then image or video stored in app's private directory may
@@ -10551,6 +10551,7 @@ public class MediaProvider extends ContentProvider {
      * the caller does not have special access.
      */
     private boolean isPrivatePackagePathNotAccessibleByCaller(String path) {
+        path = FileUtils.normalizeAndFilterDefaultIgnorableCodepoints(path);
         // Files under the apps own private directory
         final String appSpecificDir = extractPathOwnerPackageName(path);
 
@@ -10563,7 +10564,7 @@ public class MediaProvider extends ContentProvider {
         if (isExternalMediaDirectory(path)) {
             return false;
         }
-        return !isUidAllowedAccessToDataOrObbPathForFuse(mCallingIdentity.get().uid, path);
+        return !isUidAllowedAccessToDataOrObbPath(mCallingIdentity.get().uid, path);
     }
 
     private boolean shouldBypassDatabaseAndSetDirtyForFuse(int uid, String path) {
@@ -11404,6 +11405,11 @@ public class MediaProvider extends ContentProvider {
 
     @Keep
     public boolean isUidAllowedAccessToDataOrObbPathForFuse(int uid, String path) {
+        return isUidAllowedAccessToDataOrObbPath(uid,
+                FileUtils.normalizeAndFilterDefaultIgnorableCodepoints(path));
+    }
+
+    private boolean isUidAllowedAccessToDataOrObbPath(int uid, String path) {
         final LocalCallingIdentity token =
                 clearLocalCallingIdentity(getCachedCallingIdentityForFuse(uid));
         try {
